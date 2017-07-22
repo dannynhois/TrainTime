1463e702870b24622a7ddb29eeb7efdbfec962ef
Danny Nhoisaykham <danny@MBP.attlocal.net>
2017-07-22 14:03:06 -0500

    added add train form

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,58 +7,41 @@ var config = {
   storageBucket: '',
   messagingSenderId: '67730324689'
 };
-var trainId = 'two';
-
-// var trains = [
-//   {
-//     name: 'Bullet',
-//     destination: 'Houston',
-//     frequency: 35,
-//     // arrival_time: new Date(2017, 6, 19, 20, 24, 0)
-//     first_train_time: "04:30"
-//   },
-//   {
-//     name: 'Oregon Trail',
-//     destination: 'Portland',
-//     frequency: 16,
-//     first_train_time: "10:30"
-//   }
-// ];
 
 var trains = [];
+var btnSubmit = document.getElementById('submit');
 
 firebase.initializeApp(config);
 var db = firebase.database();
 var trainRefresher;
 
-
 var dbObject;
+loadDatabase();
 
 // read database and assign to trains array
-db.ref('trains/').once('value').then(snapshot=>{
-  dbObject = snapshot.val();
-  for(key in dbObject){
-    trains.push(dbObject[key]);
-  }
+function loadDatabase () {
+  db.ref('trains/').once('value').then(snapshot => {
+    dbObject = snapshot.val();
+    for (key in dbObject) {
+      trains.push(dbObject[key]);
+    }
 
-  //populate HTML and create interval to run every minute
-  populateTable();
-  trainRefresher = setInterval(populateTable,60000);
-})
-
-//train parameter is an object
-function addTrain(train) {
-  db.ref('trains/').push(train);
-  trainId++;
+    // populate HTML and create interval to run every minute
+    populateTable();
+    trainRefresher = setInterval(populateTable, 60000);
+  });
 }
 
-
+// train parameter is an object
+function addTrain (train) {
+  db.ref('trains/').push(train);
+  trains.push(train);
+}
 
 function populateTable () {
-  console.log('populateTable');
   var divTable = document.getElementById('train-times');
 
-  var html = `<table class='table table-striped table-bordered'>
+  var html = `<table class='table table-striped'>
                 <thead>
                   <tr>
                     <th>Train Name</th>
@@ -72,19 +55,19 @@ function populateTable () {
 
   // loop through object and populate table body
   trains.forEach(train => {
-    if(!train){
+    if (!train) {
       return;
     }
     var minutesAway = moment(train.first_train_time, 'HH:mm').diff(moment(), 'minutes');
     var arrivalTime = moment(train.first_train_time, 'HH:mm').format('HH:mm');
 
-    //logic for next arrival
-    while(minutesAway < 0){
+    // logic for next arrival
+    while (minutesAway < 0) {
       arrivalTime = moment(arrivalTime, 'HH:mm').add(train.frequency, 'minutes').format('HH:mm');
       minutesAway = moment(arrivalTime, 'HH:mm').diff(moment(), 'minutes');
     }
 
-    //convert from military to am/pm
+    // convert from military to am/pm
     arrivalTime = moment(arrivalTime, 'HH:mm').format('hh:mm A');
     // console.log(moment());
     html += `<tr>
@@ -99,3 +82,20 @@ function populateTable () {
   html += '</table>';
   divTable.innerHTML = html;
 }
+
+// submit new train to database
+btnSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // get values of form on click
+  var newTrain = {
+    name: document.getElementById('train-name').value,
+    destination: document.getElementById('destination').value,
+    frequency: document.getElementById('frequency').value,
+    first_train_time: document.getElementById('first-train').value
+  };
+
+  // add train and update
+  addTrain(newTrain);
+  populateTable();
+});
